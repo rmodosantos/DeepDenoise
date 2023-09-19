ea1b7fceca81548307fa9120ddbb7fc640070217
rmodosantos <ricardo.m.o.santos@gmail.com>
2023-09-19 21:49:44 +0200

    Update test

--- utils/deconv.py
+++ utils/deconv.py
@@ -2,90 +2,25 @@ import numpy as np
 from scipy.signal import welch
 from scipy.optimize import minimize_scalar
 
-def GetSn(Y, range_ff=[0.25, 0.5], method='logmexp'):
-    # if len(Y.shape) == 1:
-    #     Y = Y.reshape(-1, 1)
-    # else:
-    #     Y = Y.T
-    print(np.shape(Y))
-    psdx, ff = welch(Y, axis=0)
-    indf = np.logical_and(ff >= range_ff[0], ff <= range_ff[1]).squeeze()
-    
-    print(np.shape(indf))
-    if method == 'mean':
-        sn = np.sqrt(np.mean(psdx[indf] / 2))
-    elif method == 'median':
-        sn = np.sqrt(np.median(psdx[indf] / 2))
-    elif method == 'logmexp':
-        sn = np.sqrt(np.exp(np.mean(np.log(psdx[indf] / 2))))
-    else:
-        print('Wrong method! Using logmexp instead.')
-        sn = np.sqrt(np.exp(np.mean(np.log(psdx[indf] / 2))))
-    
-    return sn
-
-def update_g(y, active_set, lam):
-    len_active_set = active_set.shape[0]
-    y = y.reshape(-1, 1)
-    maxl = np.max(active_set[:, 3])
-    c = np.zeros_like(y)
-    
-    def rss_g(g):
-        h = np.exp(np.log(g) * np.arange(maxl+1))
-        hh = np.cumsum(h * h)
-        yp = y - lam * (1 - g)
-        for ii in range(len_active_set):
-            li = int(active_set[ii, 3])
-            ti = int(active_set[ii, 2])
-            idx = np.arange(ti, ti + li)
-            tmp_v = np.maximum(np.sum(yp[idx] * h[:li]) / hh[li], 0)
-            c[idx] = tmp_v * h[:li]
-        res = y - c
-        rss = np.sum(res * res)
-        return rss
-    
-    result = minimize_scalar(rss_g, bounds=(0, 1), method='bounded')
-    g = result.x
-    h = np.exp(np.log(g) * np.arange(maxl+1))
-    hh = np.cumsum(h * h)
-    yp = y - lam * (1 - g)
-    for m in range(len_active_set):
-        li = int(active_set[m, 3])
-        ti = int(active_set[m, 2])
-        idx = np.arange(ti, ti + li)
-        active_set[m, 0] = np.sum(yp[idx] * h[:li])
-        active_set[m, 1] = hh[li]
-    
-    c, s, active_set = oasisAR1(y, g, lam, active_set=active_set)
-    
-    return c, active_set, g, s
-
-def foopsi_oasisAR1(y, g, lam, optimize_b, optimize_g, decimate, maxIter):
-    T = len(y)
-    
-    if decimate > 1:
-        # To be done: Implement decimation
-        pass
-    
-    if not optimize_b:
-        b = 0
-        solution, spks, active_set = oasisAR1(y, g, lam)
-    else:
-        # Optimize baseline here
-        pass
-    
-    c = solution
-    s = spks
-    
-    return c, s, b, g, active_set
+# This file contains a set of functions used to deconvolute fluorescence ACh recordings.
+# The code was adopted from the CalmAn repository at: https://github.com/flatironinstitute/CaImAn
+# The scientific paper describing the aplpication of this deconvolution approach can be found at:
+# https://journals.plos.org/ploscompbiol/article?id=10.1371/journal.pcbi.1005423#sec002
 
 
-def oasisAR1(y, g=None, lam=0, smin=0, active_set=None):
+def oasisAR1(y, g, lam=0, smin=0, active_set=None):
+    """
+    Applies an Active Set method for a 1st order auto-regressive model to deconvolve a signal.
+    
+    Parameters:
+    - y (ndarray): Input signal.
+    - g (float): gamma parameter that models fluorescence impulse response (usually between 0.9 and 1
+    )
+    - lam (float): penalty parameter
+    - smin (float): minimum spike size
+    """
     y = y.reshape(-1, 1)
     T = len(y)
-    
-    if g is None:
-        g = estimate_time_constant(y)
     
     if active_set is None:
         len_active_set = T
@@ -98,34 +33,31 @@ def oasisAR1(y, g=None, lam=0, smin=0, active_set=None):
             np.arange(1, T+1)
         ])
         
-        print([y[-1,0] - lam, 1, T, 1, T - 1, np.nan])
         active_set[-1, :] = [y[-1,0] - lam, 1, T, 1, T - 1, np.nan]
         active_set[0, 4] = np.nan
     else:
         len_active_set = active_set.shape[0]
         active_set[:, 4] = np.hstack([np.nan, np.arange(0, len_active_set-1)])
         active_set[:, 5] = np.hstack([np.arange(1, len_active_set), np.nan])
-    print(active_set[-3:,5])
 
     idx = np.ones(len_active_set, dtype=bool)
     
     ii = 0
     ii_next = int(active_set[ii, 5])
     while not np.isnan(ii_next):
-        #print((1,ii_next))
+        
         while (not np.isnan(ii_next)) and (
             active_set[ii_next, 0] / active_set[ii_next, 1] >=
             active_set[ii, 0] / active_set[ii, 1] * g ** active_set[ii, 3] + smin
         ):
             active_set[ii_next, 4] = ii
             ii = int(ii_next)
-            #ii_next = int(active_set[ii, 5])
+        
             if not np.isnan(active_set[ii, 5]):
                 ii_next = int(active_set[ii, 5])
             else:
                 ii_next = active_set[ii, 5]
 
-            #print((2,ii_next))
             
         if np.isnan(ii_next):
             break
@@ -142,7 +74,6 @@ def oasisAR1(y, g=None, lam=0, smin=0, active_set=None):
         else:
             ii_next = active_set[ii, 5]
         
-        #print((3,ii_next,ii))
 
         if not np.isnan(active_set[ii, 4]):
             ii_prev = int(active_set[ii, 4])
@@ -174,8 +105,6 @@ def oasisAR1(y, g=None, lam=0, smin=0, active_set=None):
     
     active_set = active_set[idx, :]
     len_active_set = active_set.shape[0]
-    
-    print(len_active_set)
 
     c = np.zeros_like(y)
     s = np.zeros_like(y)
@@ -183,32 +112,17 @@ def oasisAR1(y, g=None, lam=0, smin=0, active_set=None):
     for ii in range(len_active_set):
         t0 = int(active_set[ii, 2])
         tau = int(active_set[ii, 3])
-        #print((t0,tau))
-        #print(np.maximum(0, active_set[ii, 0] / active_set[ii, 1]) * (g ** np.arange(0, tau)))
-        #print(np.maximum(0, active_set[ii, 0] / active_set[ii, 1]) * (g ** np.arange(0, tau)).reshape(-1,1))
         c[t0:(t0 + tau)] = np.maximum(0, active_set[ii, 0] / active_set[ii, 1]) * (g ** np.arange(0, tau)).reshape(-1,1)
     
     s[active_set[1:, 2].astype(int) - 1] = c[active_set[1:, 2].astype(int) - 1] - g * c[active_set[1:, 2].astype(int) - 2]
     
     return c, s, active_set
 
-def estimate_time_constant(y):
-    # Implementation of the estimate_time_constant function
-    pass
-
-
 
 def deconvolveCa_py(y, g, lam):
     y = y.reshape(-1, 1)
-    win = 200
-    sn = GetSn(y)
     
-    c = y.copy()
-    s = y.copy()
+    c, s, _ = oasisAR1(y, g, lam)
     
-    c, s, b, g, active_set = foopsi_oasisAR1(y, g, lam, optimize_b=0, optimize_g=0, decimate=1, maxIter=10)
-    
-    options = {'b': b, 'g': g}
-    
-    return c, s, options
+    return c, s
 
--- utils/spectralfits.py
+++ utils/spectralfits.py
@@ -5,48 +5,84 @@ from scipy.optimize import curve_fit
 from scipy.signal import butter, filtfilt
 import scipy.io as sio
 
+# This file contains a set of functions used in the CNN test analysis
+
 def fit_ACh_spectra(yspec, AChefnorm, Hb, HbO2):
-    # Assumes the standard wavelength vector from 350 to 1000 nm from spectrometer data
     
+    """Function to fit spectral features on a fluorescence signal.
+    Inputs:
+    yspec: Fluorescence data
+    By default it assumes:
+    ACh: Spectral profile of Acetylcholine effect in fluorescence
+    Hb: deoxy-hemoglobin absorption spectrum
+    HbO2: oxy-hemoglobin absorption spectrum 
+    
+    Output is an array where each column represent the time-series of intensity of a given feature:
+    1 - Offset after mean normalization (inside exponent)
+    2 - HbO2 intensity
+    3 - Hb intensity
+    4 - ACh intensity
+    5 - Offset before mean normalization (outside exponent)"""
+    
+    # Calculate mean fluorescence for normalization
     neutralinvivo = np.mean(yspec, axis=0)
-    #sig_n = yspec[:, sw] / neutralinvivo.reshape(1, -1)
+    # Normalize
     sig_n = yspec / neutralinvivo.reshape(1, -1)
     
-
+    # Reshape inputs
     AChefnorm = AChefnorm.reshape(-1,152)
     HbInterp = Hb.reshape(-1,152)
     HbO2Interp = HbO2.reshape(-1,152)
     
+    # Define fitting function
     def mF(x, m1, m2, m3, m4, m5):
         return 1. * np.exp(m1 - x[0,:] * m2 - x[1,:] * m3 + x[2,:] * m4) + m5 / neutralinvivo
     
-    yfity = sig_n
-    
-    mfit = np.zeros((yfity.shape[0], 5))
-    Ly = yfity.shape[0]
+    mfit = np.zeros((sig_n.shape[0], 5))  #fitting array
+    Ly = sig_n.shape[0]
     
     # Fit the model
     for i in range(Ly):
-        if np.sum(yfity[i]) > 0:
-            #print(np.shape(np.concatenate((HbO2Interp, HbInterp, AChefnorm),axis=1))
+        if np.sum(sig_n[i]) > 0:
             input_data = np.concatenate((HbO2Interp, HbInterp, AChefnorm),axis=0)
-            #print(np.shape(input_data))
-            popt, _ = curve_fit(mF, input_data, yfity[i], p0=[0.1, 0.1, 0.1, 0.1, 1])
+            popt, _ = curve_fit(mF, input_data, sig_n[i], p0=[0.1, 0.1, 0.1, 0.1, 1])
             mfit[i] = popt
     
     return mfit
 
 
-
-
 def ButFilter(x, n, wn, flag):
-    """Function that filters the signal"""
+    """
+    Apply Butterworth filter to input signal 'x'.
+
+    Parameters:
+    - x: Input signal.
+    - n: Filter order.
+    - wn: Cutoff frequency or frequencies.
+    - flag: Filter type ('low', 'high', 'band', 'stop').
+
+    Returns:
+    - y: Filtered output signal.
+    """
+
     b, a = butter(n, wn, btype=flag, analog=False)
     y = filtfilt(b, a, x, axis=0)
     
     return y
 
+
 def get_elements_in_periods(periods, A):
+    """
+    Extract elements from a vector that fall within specified intervals.
+
+    Parameters:
+    - periods (list of tuples or numpy array): List of intervals array specifying start and end of each period.
+    - A (array-like): Input vector from which elements within intervals are to be extracted.
+
+    Returns:
+    - elements (numpy array): Array containing elements of 'A' within the specified intervals.
+    - indices (numpy array): Boolean array marking the indices of 'A' that fall within the intervals.
+    """
     result = []
     inds = []
     for start, end in periods:
@@ -55,15 +91,30 @@ def get_elements_in_periods(periods, A):
         inds.extend(mask)
     return np.array(result),np.array(inds)
 
+
 def denoise(data, model_path, denoised_path, network_depth=20, dev='cpu',save_file=True):
-    """Denoise a fluorescence signal using the specified denoising network"""
+    """
+    Denoise a fluorescence signal using the specified denoising network.
+
+    Parameters:
+    - data (ndarray): Input fluorescence signal.
+    - model_path (str): Path to the trained denoising model checkpoint.
+    - denoised_path (str): Path to save the denoised data (MATLAB .mat file).
+    - network_depth (int): Depth of the denoising network.
+    - dev (str): Device for computation ('cpu' or 'cuda' for GPU).
+    - save_file (bool): Whether to save the denoised data as a file.
+
+    Returns:
+    - data_denoised (ndarray): Denoised fluorescence signal.
+    """
 
     from os import remove
-
+    
+    # Move data to device
     dev=torch.device(dev)
     test_data = torch.tensor(data, dtype=float, device=dev)
 
-
+    # Create CNN class
     class DnCNN(nn.Module):
         def __init__(self, depth=17, n_filters=64, kernel_size=3, n_channels=1,padding=1):
                 """Pytorch implementation of DnCNN. Implementation followed the original paper [1]_. Authors original code can be
@@ -120,19 +171,33 @@ def denoise(data, model_path, denoised_path, network_depth=20, dev='cpu',save_fi
             return out
 
     def reformat_data(data,window,SampRate=5):
-        """Function used to reformat the data according to the denoising model input"""
-        window_sample = np.round(window*0.8)
-        times = torch.arange(window*SampRate,len(data)-window*SampRate,window_sample*SampRate*2,dtype=int)
-        datar = torch.zeros((times.shape[0]-1,1,2*window*SampRate+1,data.shape[1]),device=dev)
-        datar_original = torch.zeros((times.shape[0]-1,1,2*window*SampRate+1,data.shape[1]),device=dev)
-        #mean_data = torch.zeros((times.shape[0],data.shape[1]))
-        last_sample = torch.zeros((1,1,len(data)-(times[-1]-int(window*SampRate)),data.shape[1]),device=dev)
-        last_sample_original = torch.zeros((1,1,len(data)-(times[-1]-int(window*SampRate)),data.shape[1]),device=dev)
+        """
+        Reformat the input data according to the denoising model's input requirements.
 
+        Args:
+            data (ndarray): Input fluorescence signal.
+            window (int): Size of the data window.
+            SampRate (int, optional): Sampling rate. Defaults to 5.
+
+        Returns:
+            datar (torch.Tensor): Reformatted data suitable for the denoising model.
+            last_sample (torch.Tensor): The last segment of reformatted data.
+        """
+        window_sample = np.round(window*0.8)  #narrower denoising window to avoid border effects
+        
+        # Define the centers of data windows
+        times = torch.arange(window*SampRate,len(data)-window*SampRate,window_sample*SampRate*2,dtype=int)
+        
+        # Pre-allocate output arrays
+        datar = torch.zeros((times.shape[0]-1,1,2*window*SampRate+1,data.shape[1]),device=dev)
+        last_sample = torch.zeros((1,1,len(data)-(times[-1]-int(window*SampRate)),data.shape[1]),device=dev)
+        
+        # Reformat equally-sized data
         for i,t in enumerate(times[:-1]):
             data_seg = data[t-window*SampRate:t+window*SampRate+1,:]
             datar[i,0,:,:] = data_seg
-              
+        
+        # Reformat remaining data sample
         sample = data[times[-1]-int(window*SampRate):,:]
         last_sample[0,0,:,:] = sample
   
@@ -141,46 +206,53 @@ def denoise(data, model_path, denoised_path, network_depth=20, dev='cpu',save_fi
 
     # Denoise the recording
     def Denoise_recording(net,data, last_data, path, window, rescale, noise_scale=1, SampRate=5):
-        """Apply denoising model and convert data to original format"""
+        """
+        Apply denoising model to the input data and convert it to the original format.
 
+        Args:
+            net (nn.Module): Denoising neural network.
+            data (torch.Tensor): Reformatted input data.
+            last_data (torch.Tensor): Last segment of input data.
+            path (str): Path to save the denoised data (MATLAB .mat file).
+            window (int): Size of the data window.
+            rescale (float): Rescaling factor.
+            noise_scale (float, optional): Noise scaling factor. Defaults to 1.
+            SampRate (int, optional): Sampling rate. Defaults to 5.
+        """
+        # Size of the border to exclude in each denoising window
         col_border = int(np.round(window-window*rescale)*SampRate)
 
         mat_data = []
         
         for i in range(len(data)):
             if i==0:
-
-            #with h5py.File(path, 'w') as f:
-
+                # Denoise window
                 tm_inst = data[i,:,:,:].unsqueeze(0)*noise_scale-net(data[i,:,:,:].unsqueeze(0)*noise_scale)
                 tm_inst = tm_inst/noise_scale
                 tm_inst = tm_inst[:,:,:-col_border-1,:]
                 tm_inst = tm_inst.view(tm_inst.shape[2]*tm_inst.shape[0],tm_inst.shape[3])
 
-                #dset = f.create_dataset('data', data=tm_inst.cpu().detach().numpy(), dtype='f',maxshape=(None,152),chunks=(1,152))  # 'f' stands for float32
                 mat_data.append(tm_inst.cpu().detach().numpy())
                 
             else:
-            
-            
+                # Denoise window
                 tm_inst = data[i,:,:,:].unsqueeze(0)*noise_scale-net(data[i,:,:,:].unsqueeze(0)*noise_scale)
                 tm_inst = tm_inst/noise_scale
                 tm_inst = tm_inst[:,:,col_border:-col_border-1,:]
                 tm_inst = tm_inst.view(tm_inst.shape[2]*tm_inst.shape[0],tm_inst.shape[3])
 
-               
                 mat_data.append(tm_inst.cpu().detach().numpy())
                     
-        
+        # Denoise last data window
         tm_inst = last_data[:,:,col_border:,:]*noise_scale-net(last_data[:,:,col_border:,:]*noise_scale)
         tm_inst = tm_inst/noise_scale
         tm_inst = tm_inst.view(tm_inst.shape[2]*tm_inst.shape[0],tm_inst.shape[3])
         mat_data.append(tm_inst.cpu().detach().numpy())
-            
+        
+        # Save to file
         sio.savemat(path, {"data": np.concatenate(mat_data, axis=0)})
 
-    
-
+    # Reformat data
     data_net,last_data_net = reformat_data(test_data,10)
     
     # load model checkpoint
